c1292f54ccbcb5bcf456bad63148684befd15d10
Horner735 <0misssquishy0@gmail.com>
2015-06-15 10:04:26 -0400

    hornerj - initial version of cat calculator
    This is the cat calculator that we worded on the first week of class.

--- Cat Age Calculater/ViewController.swift
+++ Cat Age Calculater/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblCalcAge: UILabel!
+    @IBOutlet weak var txtAge: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func Submit(sender: AnyObject) {
+        println("Hello World")
+        var enteredAge = txtAge.text.toInt()
+        if enteredAge != nil {
+          var catYears = enteredAge! * 7
+          lblCalcAge.text = "Your Cat is \(catYears) in cat years"
+        }
+        else {
+          lblCalcAge.text = "Please enter a number in the box."
+          }
+        
+        
     }
 
 
